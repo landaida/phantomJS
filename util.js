f4d58fb632dafc4206b7be887e0a1bd6d4f3ceac
landaida <landaida.1@gmail.com>
2016-03-23 00:35:28 -0300

    Before remove array

--- util.js
+++ util.js
@@ -1,8 +1,6 @@
-console.log('inside util');
-returnTitle = function() { console.log('hola'); return 'hola'; };
 
 waitFor = function(testFx, onReady, timeOutMillis) {
-  var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 60000, //< Default Max Timout is 3s
+  var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 5000, //< Default Max Timout is 3s
     start = new Date().getTime(),
     condition = false,
     interval = setInterval(function() {
@@ -15,14 +13,47 @@ waitFor = function(testFx, onReady, timeOutMillis) {
           console.log("'waitFor()' timeout");
           if(phantom)
             phantom.exit(1);
-          else if(my_exit)
-            my_exit();
+          else if(my_exit){
+            window.callPhantom({
+              exit: true
+            });
+          }
         } else {
           // Condition fulfilled (timeout and/or condition is 'true')
           // console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-          typeof(onReady) === "string" ? eval(onReady): onReady(); //< Do what it's supposed to do once the condition is fulfilled
           clearInterval(interval); //< Stop this interval
+          typeof(onReady) === "string" ? eval(onReady): onReady(); //< Do what it's supposed to do once the condition is fulfilled
         }
       }
     }, 250); //< repeat check every 250ms
 };
+
+
+function takeScreenShot(first_char) {
+  var title = first_char ? first_char : '', d = new Date();
+  title += d.toLocaleTimeString().split(' ')[0].replace(/\:/g, ' ');
+  window.callPhantom({
+    render: true,
+    title: title
+  });
+}
+
+function my_exit() {
+  window.callPhantom({
+    exit: true
+  });
+}
+
+function fractionToDecimal(number){
+  return parseFloat(eval(number)).toFixed(2)
+}
+
+function objToString(obj) {
+  var str = '';
+  for (var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      str += p + '::' + obj[p] + '\n';
+    }
+  }
+  return str;
+}
