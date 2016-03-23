7ff113d9c355c0d302d6e2306a22c9a69cfd0d98
landaida <landaida.1@gmail.com>
2016-03-23 09:40:46 -0300

    Fix errors and change the way of getting plays

--- util.js
+++ util.js
@@ -1,29 +1,42 @@
 
-waitFor = function(testFx, onReady, timeOutMillis) {
+waitFor = function(testFx, onReady, timeOutMillis, showConsole) {
   var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 5000, //< Default Max Timout is 3s
     start = new Date().getTime(),
     condition = false,
     interval = setInterval(function() {
-      if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
-        // If not time-out yet and condition not yet fulfilled
-        condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-      } else {
-        if (!condition) {
-          // If condition still not fulfilled (timeout but condition is 'false')
-          console.log("'waitFor()' timeout");
-          if(phantom)
-            phantom.exit(1);
-          else if(my_exit){
-            window.callPhantom({
-              exit: true
-            });
+      try {
+        if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
+          // If not time-out yet and condition not yet fulfilled
+          condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+          if(showConsole){
+            var d = new Date()
+            ,title = 'insideWaitFor' + d.toLocaleTimeString().split(' ')[0].replace(/\:/g, ' ');
+            console.log('insideWaitFor', condition, timeOutMillis);
+            // window.callPhantom({
+            //   render: true,
+            //   title: title
+            // });
           }
         } else {
-          // Condition fulfilled (timeout and/or condition is 'true')
-          // console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-          clearInterval(interval); //< Stop this interval
-          typeof(onReady) === "string" ? eval(onReady): onReady(); //< Do what it's supposed to do once the condition is fulfilled
+          if (!condition) {
+            // If condition still not fulfilled (timeout but condition is 'false')
+            console.log("'waitFor()' timeout");
+            if(phantom)
+            phantom.exit(1);
+            else if(my_exit){
+              window.callPhantom({
+                exit: true
+              });
+            }
+          } else {
+            // Condition fulfilled (timeout and/or condition is 'true')
+            // console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+            clearInterval(interval); //< Stop this interval
+            typeof(onReady) === "string" ? eval(onReady): onReady(); //< Do what it's supposed to do once the condition is fulfilled
+          }
         }
+      } catch (e) {
+        console.log('Error', err.message);
       }
     }, 250); //< repeat check every 250ms
 };
